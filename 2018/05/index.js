dab8d26b2e919a6a43ce0b5439cca04de7d9408a
spencer kelly <spencermountain@gmail.com>
2018-12-19 17:39:39 -0500

    move baseball to non-draft

--- 2018/05/index.js
+++ 2018/05/index.js
@@ -1,6 +1,6 @@
 // const somehow = require('somehow');
 const somehow = require('/Users/spencer/mountain/somehow/src');
-let teams = require('./data/attendance')
+let teams = require('./data/performance')
 let divisions = require('./data/divisions')
 
 const colors = [
@@ -19,17 +19,21 @@ const doDivision = function(key, id) {
     height: 200,
     width: 1000
   });
-  w.text(key + ':').font(16).color('olive').set('-10%, 50%')
   divisions[key].forEach((team, i) => {
     let line = w.line().width(2).color(colors[i] || 'blue')
     let games = teams[team].games
+    games = games.filter((g) => g[1] !== null)
     if (games.length > 0) {
       line.set(games)
       w.text(team).font(10).color(colors[i]).set([games[games.length - 1]])
     }
   })
 
-  w.y.fit(0, 60000);
+  w.text(key + ':').font(16).color('olive').set('-10%, 50%')
+  w.line().dotted(true).color('lightgrey').width(1).set([['0px', 0], ['100%', 0]])
+
+  // w.line().dotted(true).color('lightgrey').width(1).set([['Sept 30, 2018', 0], ['Sept 30, 2018', 30]])
+  w.y.fit(-35, 35);
   w.x.fit('Mar 20 2018', 'Oct 20 2018');
 
   let el = document.querySelector(id);
---
+++ 2018/05/scripts/performance.js
@@ -0,0 +1,34 @@
+const wtfMLB = require('wtf-mlb')
+let teams = require('../data/teams')
+// teams = teams.slice(0, 3)
+const year = 2018
+let results = {}
+
+const doTeam = function(i, cb) {
+  wtfMLB.fetch(teams[i], year).catch(console.log).then(data => {
+    if (!data) {
+      console.log(teams[i])
+    } else {
+      let games = data.games.map((g) => {
+        let fifty = g.record.games / 2
+        let underOver = g.record.wins - fifty
+        let date = g.date.replace(/\(.*?\)/, '') + ' ' + year
+        return [date, underOver]
+      })
+      games = games.filter(g => g[1] !== null)
+      results[teams[i]] = {
+        team: teams[i],
+        year: year,
+        games: games
+      }
+    }
+    i += 1
+    if (teams[i]) {
+      return doTeam(i, cb)
+    }
+    return cb()
+  })
+}
+doTeam(0, () => {
+  console.log(JSON.stringify(results, null, 2))
+})
