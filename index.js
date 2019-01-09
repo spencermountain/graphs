5d0800ceb6e1d5d4b39d7bf13565b09c4206f1cf
spencer kelly <spencermountain@gmail.com>
2019-01-09 18:44:23 -0500

    update homepage

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ let result = [];
 years.forEach((y) => {
   let posts = manifest[y].map((o) => {
     let path = y + '/' + o.num;
-    return h`<a href="${path}" class="left flex-start w30 m3" >
-    <span class="olive" >${o.num}</span>
-    <span class="link"> / ${o.title}</span>
+    return h`<a href="${path}" class="left mw20 m3" >
+    <div class="">
+      <span class="olive" >${o.num}</span>
+      <span class="link"> / ${o.title}</span>
+    </div>
     <img class="block" style="margin-left:95px; max-width:450px; max-height:100px;" src="${path + '/' + o.thumb}" />
     </a>
     `;
   });
-  result.push(h`<h3 class="left dimgrey">${y}</h3>`);
+  result.push(h`<h3 class="">${y}</h3>`);
   result = result.concat(posts);
 });
 
