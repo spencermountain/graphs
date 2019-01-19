1de407b60a2cfe0856daf369689ba249c4e074c8
spencer kelly <spencermountain@gmail.com>
2019-01-19 18:32:38 -0500

    saturday night release

--- assets/bundle.js
+++ assets/bundle.js
@@ -75,6 +75,10 @@ module.exports = {
     num: '02',
     title: 'History of the NHL',
     thumb: 'thumb.png'
+  }, {
+    num: '03',
+    title: 'Rain in Toronto',
+    thumb: 'thumb.png'
   }]
 };
 
--- manifest.js
+++ manifest.js
@@ -36,6 +36,11 @@ module.exports = {
       num: '02',
       title: 'History of the NHL',
       thumb: 'thumb.png'
+    },
+    {
+      num: '03',
+      title: 'Rain in Toronto',
+      thumb: 'thumb.png'
     }
   ]
 };
