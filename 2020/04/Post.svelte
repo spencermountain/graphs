05bff4597935ce3f4333462059da9277fc47aaec
spencer kelly <spencermountain@gmail.com>
2020-05-05 16:09:17 -0400

    more work on skydome roof

--- 2020/04/Post.svelte
+++ 2020/04/Post.svelte
@@ -7,10 +7,11 @@
     Line,
     colors,
     Ticks,
+    Section,
     Now,
   } from '/Users/spencer/mountain/somehow-timeline/src'
   import games from './data/games'
-  let years = Object.keys(games).reverse()
+  let years = Object.keys(games) //.reverse()
   export let title = 'Skydome roof by year'
   export let sub = ''
   let start = 'March 20 2020'
@@ -19,6 +20,18 @@
   let num = '04'
   let d = new Date()
   let today = d.toISOString()
+  let byYear = {}
+  years.forEach(y => {
+    y = String(y)
+    let all = games[y].filter(g => g.is_open) || []
+    let first = all.find(g => g.is_open) || {}
+    byYear[y] = {
+      first: first.date,
+      count: all.length,
+      total: games[y].length,
+    }
+  })
+  console.log(byYear)
 </script>
 
 <style>
@@ -37,30 +50,36 @@
       </Column>
       <Column width="75px">
         <Ticks every="week" size="8px" color="lightgrey" underline={false} />
-
       </Column>
 
       {#each years as year, i}
-        <Column width="85px" label={year}>
+        <Column width="35px" label={year}>
           {#if year === '2020'}
             <Line
-              space="15px"
               width="3px"
               start={`2020-03-29`}
-              end={`2020-07-20`}
-              size="14px"
-              label="covid"
-              color={'yellow'} />
+              end={today}
+              size="12px"
+              opacity="0.4"
+              color="lightgrey" />
           {/if}
           {#each games[year] as game}
             <Line
               space="15px"
-              width="3px"
+              width="5px"
               start={`2020-${game.date}`}
               duration="18 hours"
               size="14px"
               color={game.is_open ? 'steelblue' : 'lightsteelblue'} />
           {/each}
+          <Section date="Nov 5 2020">
+            <b style="font-size:20px">{byYear[year].count}</b>
+            <span
+              style="font-size:11px; margin-left:17px; position:relative; top:-10px;
+              color:lightgrey;">
+              /{byYear[year].total}
+            </span>
+          </Section>
         </Column>
       {/each}
       <!-- <Column width="25px">
