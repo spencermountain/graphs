b9a24f3361abd09d10bca6d1abd1d89407a8902d
spencer kelly <spencermountain@gmail.com>
2020-05-05 12:58:44 -0400

    got dome data to 2012

---
+++ drafts/skydome-roof/scripts/getGames.js
@@ -0,0 +1,28 @@
+//
+const axios = require('axios')
+const $ = require('cheerio')
+
+const fetchGames = async function (year) {
+  let url = ` https://www.baseball-reference.com/teams/TOR/${year}-schedule-scores.shtml#all_results`
+  return axios.get(url).then((res) => {
+    let games = []
+    console.log(res.data)
+    $('td[data-stat="boxscore"]', res.data).each(function () {
+      let href = $(this).html()
+      console.log(href)
+      // href = href.replace('/boxscores/', '')
+      games.push(href)
+    })
+    // console.log(games)
+    return games
+  })
+}
+
+// let els = document.querySelectorAll('td[data-stat="boxscore"]')
+// let arr = []
+// for (let i = 0; i < els.length; i += 1) {
+//   console.log(i)
+//   arr.push(els[i].querySelector('a').href)
+// }
+
+console.log(fetchGames(2012))
---
+++ drafts/skydome-roof/scripts/getRoof.js
@@ -0,0 +1,45 @@
+const games = require('./games')
+const axios = require('axios')
+const $ = require('cheerio')
+let results = {}
+//
+const fetchGame = async function (id) {
+  let url = `https://www.baseball-reference.com/boxes/TOR/${id}.shtml`
+  return axios
+    .get(url)
+    .then((response) => {
+      let html = response.data
+      let str = html.match(/weather.{0,100}/gi)[0] || ''
+      // console.log(str)
+      if (str.match(/in dome/i)) {
+        results[id] = false
+        console.log(`${id}\t${false}`)
+      } else {
+        results[id] = true
+        console.log(`${id}\t${true}`)
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  // let href = $(this).text || ''
+  //   console.log(href)
+  //   // href = href.replace('/boxscores/', '')
+  //   games.push(href)
+  // })
+  // return games
+}
+;(async () => {
+  for (let i = 0; i < games.length; i += 1) {
+    await fetchGame(games[i])
+  }
+  // await fetchGame(games[200])
+  console.log(JSON.stringify(results, null, 2))
+})()
+// ;(async () => {
+//   try {
+//     await fetchGame('https://www.baseball-reference.com/boxes/TOR/TOR201906050.shtml')
+//   } catch (e) {
+//     console.log(e)
+//   }
+// })()
