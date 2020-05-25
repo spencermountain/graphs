6cef977c34f3a2e85b095835a9a6b1f72b5926e5
spencer kelly <spencermountain@gmail.com>
2020-05-25 12:55:32 -0400

    atlas, shuttle, and saturn5 done

---
+++ drafts/rocket-launches/scripts/fetch.js
@@ -0,0 +1,36 @@
+const wtf = require('/Users/spencer/mountain/wtf_wikipedia')
+
+let all = []
+;(async () => {
+  // saturn 5
+  //   all = []
+  //   let doc = await wtf.fetch('Saturn_V')
+  //   let table = doc.section('Saturn V vehicles and launches').tables(0)
+  //   let rows = table.json()
+  //   rows.forEach((row) => {
+  //     all.push({
+  //       date: row['Launch date (UTC)'].text.replace(/ [0-9:]*$/, ''),
+  //       name: row['Mission'].text,
+  //     })
+  //   })
+  //   all = all.filter((o) => o.date)
+  //   console.log(JSON.stringify(all, null, 2))
+
+  // atlas
+  let doc = await wtf.fetch('List_of_Atlas_launches_(2000–2009)')
+  doc.tables().forEach((table) => {
+    let rows = table.json()
+    console.log(rows)
+    rows.forEach((row) => {
+      console.log(row)
+      all.push({
+        // date: row['Date/Time (UTC)'].text,
+        // name: row['S/N'].text,
+        date: row['Date / time (UTC) '].text,
+        name: row['Flight №'].text,
+      })
+    })
+  })
+  //   all = all.filter((o) => o.date)
+  console.log(JSON.stringify(all, null, 2))
+})()
