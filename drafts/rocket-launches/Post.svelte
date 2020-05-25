673c9c7e3aa055485d4771c5dcb47465bdc77f5f
spencer kelly <spencermountain@gmail.com>
2020-05-25 14:46:12 -0400

    graph is rendering

--- drafts/rocket-launches/Post.svelte
+++ drafts/rocket-launches/Post.svelte
@@ -1,8 +1,25 @@
 <script>
   import Head from '../../components/Head.svelte'
   import Foot from '../../components/Foot.svelte'
-  export let title = ''
-  export let sub = ''
+
+  import {
+    Timeline,
+    Column,
+    Line,
+    colors,
+    Ticks,
+    Dash,
+  } from '/Users/spencer/mountain/somehow-timeline/src'
+
+  let year = new Date().getFullYear()
+  let start = 'Jan 1 1950'
+  let end = 'Dec 31 2030'
+  let height = 3500
+  export let title = 'Rocket Launches'
+
+  import atlas from './data/atlas.json'
+  import saturn5 from './data/saturn5.json'
+  import china from './data/china.json'
 </script>
 
 <style>
@@ -12,7 +29,30 @@
 </style>
 
 <div>
-  <Head {title} {sub} />
-  <div class="m3">hello</div>
+  <Head {title} num="07" />
+  <div class="m3">
+    <Timeline {start} {end} {height} {title}>
+      <Ticks every="decade" />
+      <Ticks every="year" size="8px" color="lightgrey" underline={false} />
+
+      <Column width="75px" label="Saturn 5">
+        {#each saturn5 as rocket}
+          <Dash start={rocket.date} color="blue" opacity="0.5" dotted={false} />
+        {/each}
+      </Column>
+
+      <Column width="75px" label="Atlas">
+        {#each atlas as rocket}
+          <Dash start={rocket.date} color="rose" opacity="0.5" dotted={false} />
+        {/each}
+      </Column>
+
+      <Column width="75px" label="China">
+        {#each china as rocket}
+          <Dash start={rocket.date} color="orange" opacity="0.5" dotted={false} />
+        {/each}
+      </Column>
+    </Timeline>
+  </div>
   <Foot {title} />
 </div>
