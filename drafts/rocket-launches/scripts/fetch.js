12026271e7b22fe9e78c6721056d211af22aa246
spencer kelly <spencermountain@gmail.com>
2020-05-25 13:40:17 -0400

    japan and china

--- drafts/rocket-launches/scripts/fetch.js
+++ drafts/rocket-launches/scripts/fetch.js
@@ -17,20 +17,27 @@ let all = []
   //   console.log(JSON.stringify(all, null, 2))
 
   // atlas
-  let doc = await wtf.fetch('List_of_Atlas_launches_(2000–2009)')
-  doc.tables().forEach((table) => {
-    let rows = table.json()
-    console.log(rows)
-    rows.forEach((row) => {
-      console.log(row)
-      all.push({
-        // date: row['Date/Time (UTC)'].text,
-        // name: row['S/N'].text,
-        date: row['Date / time (UTC) '].text,
-        name: row['Flight №'].text,
-      })
-    })
+  //   let doc = await wtf.fetch('List_of_Atlas_launches_(2000–2009)')
+  //   doc.tables().forEach((table) => {
+  //     let rows = table.json()
+  //     console.log(rows)
+  //     rows.forEach((row) => {
+  //       console.log(row)
+  //       all.push({
+  //         // date: row['Date/Time (UTC)'].text,
+  //         // name: row['S/N'].text,
+  //         date: row['Date / time (UTC) '].text,
+  //         name: row['Flight №'].text,
+  //       })
+  //     })
+  //   })
+
+  // china
+  let doc = await wtf.fetch('List_of_Long_March_launches')
+  let launches = doc.templates().filter((t) => t.template === 'launch')
+  launches.forEach((t) => {
+    all.push({ date: t.date.replace(/ [0-9:]*$/, ''), name: t.serial })
   })
-  //   all = all.filter((o) => o.date)
+
   console.log(JSON.stringify(all, null, 2))
 })()
