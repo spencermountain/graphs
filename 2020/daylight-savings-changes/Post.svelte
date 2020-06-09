2e66a77ff01aa09c66e2e54d41bf7b3130293afe
spencer kelly <spencermountain@gmail.com>
2020-06-09 15:44:02 -0400

    split dst graph by hemisphere

--- 2020/daylight-savings-changes/Post.svelte
+++ 2020/daylight-savings-changes/Post.svelte
@@ -1,7 +1,8 @@
 <script>
   import Head from '../../components/Head.svelte'
   import Foot from '../../components/Foot.svelte'
-  export let title = 'Daylight savings time changes in 2020'
+  import spacetime from 'spacetime'
+  export let title = 'DST change times, in 2020'
   export let sub = "'Eastern Standard' time"
   import data from './data'
   import {
@@ -14,13 +15,22 @@
     Label,
     colors,
   } from '/Users/spencer/mountain/somehow-timeline/src'
-  let year = 2019
+  let year = new Date().getFullYear()
   let start = 'Jan 1 '
   let end = 'Dec 31 '
   let height = 900
   let byTime = {}
   let zones = data.map((zone, i) => {
     zone.color = colors.combos.roma[i % 6]
+    zone.times.forEach(o => {
+      let a = o.start.split(/\//)
+      let s = spacetime({ month: Number(a[0]), date: a[1], year: year })
+      o.start = s.format('iso')
+
+      a = o.end.split(/\//)
+      s = spacetime({ month: Number(a[0]), date: a[1], year: year })
+      o.end = s.format('iso')
+    })
     return zone
   })
   zones = zones.sort((a, b) => {
@@ -31,35 +41,131 @@
     }
     return 0
   })
+
+  // console.log(zones)
+  let positives = []
+  let negatives = []
+  zones.forEach(z => {
+    if (z.offset < 0) {
+      negatives.push(z)
+    } else {
+      positives.push(z)
+    }
+  })
+  // add already
+  positives.reverse()
+  let already = 0
+  positives.forEach(z => {
+    z.already = already
+    z.times.forEach(t => {
+      already += t.zones.length
+    })
+  })
+  already = 0
+  negatives.forEach(z => {
+    z.already = already
+    z.times.forEach(t => {
+      already += t.zones.length
+    })
+  })
+  // negatives.reverse()
 </script>
 
 <style>
   .m3 {
     margin: 3rem;
   }
+  .grey {
+    color: #d1d1d1;
+  }
 </style>
 
 <div>
   <Head {title} {sub} num="02" />
   <div class="m3">
-    <Timeline {start} {end} {height} {title}>
-      <Ticks every="month" />
-      <Line />
-      <Now label="today" color="pink" />
-      {#each zones as zone, i}
-        {#each zone.times as time}
-          <Column width="15px">
-            <Line
-              space="15px"
-              start={time.start}
-              end={time.end}
-              color={zone.color}
-              label={time.zones.length} />
-          </Column>
-        {/each}
-      {/each}
-    </Timeline>
+    {title}
+    <div class="m3">
+      <div>Western Hemisphere</div>
+      <div class="grey">(North/South America)</div>
+      <Timeline {start} {end} {height}>
+        <Ticks every="month" />
+        <Now label="today" color="pink" />
+        <WideLabel
+          left={'150px'}
+          width="160px"
+          label={'AST'}
+          color="rgb(214, 136, 129)"
+          date={'Feb 5 ' + year} />
+        <WideLabel
+          left={'400px'}
+          width="200px"
+          label={'EST'}
+          color="rgb(139, 163, 162)"
+          date={'Feb 5 ' + year} />
+        <WideLabel
+          left={'790px'}
+          width="160px"
+          label={'CST'}
+          color="rgb(196, 171, 171)"
+          date={'Feb 5 ' + year} />
+        <WideLabel
+          left={'1020px'}
+          width="160px"
+          label={'MST'}
+          color="rgb(138, 132, 154)"
+          date={'Feb 5 ' + year} />
+        <WideLabel
+          left={'1250px'}
+          width="160px"
+          label={'PST'}
+          color="rgb(181, 176, 191)"
+          date={'Feb 5 ' + year} />
 
+        {#each negatives as zone, i}
+          <WideLabel
+            left={50 + zone.already * 18 + 'px'}
+            width="30px"
+            color={zone.color}
+            label={zone.offset + 'h'}
+            date={'march 1 ' + year} />
+          {#each zone.times as time}
+            {#each time.zones as name}
+              <Column width="8px">
+                <Line start={time.start} end={time.end} title={name} color={zone.color} />
+              </Column>
+            {/each}
+          {/each}
+        {/each}
+      </Timeline>
+    </div>
+
+    <div class="m3">
+      <div>Eastern Hemisphere</div>
+      <div class="grey">(Asia/Africa)</div>
+      <Timeline {start} {end} {height}>
+        <Ticks every="month" />
+        <Now label="today" color="pink" />
+        {#each positives as zone, i}
+          {#if !String(zone.offset).match(/\./)}
+            <WideLabel
+              left={50 + zone.already * 18 + 'px'}
+              width="30px"
+              color={zone.color}
+              label={zone.offset + 'h'}
+              date={'march 25 ' + year} />
+          {/if}
+          {#each zone.times as time}
+            {#each time.zones as name}
+              <Column width="8px">
+                <Line start={time.start} end={time.end} title={name} color={zone.color} />
+              </Column>
+            {/each}
+          {/each}
+        {/each}
+      </Timeline>
+
+    </div>
   </div>
+
   <Foot {title} />
 </div>
