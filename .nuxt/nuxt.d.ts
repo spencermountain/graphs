150697856cf4700d4d526a8e7fecfee0e4210b74
spencer kelly <spencerkelly86@gmail.com>
2026-07-10 17:13:18 -0400

    build

--- .nuxt/nuxt.d.ts
+++ .nuxt/nuxt.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="@nuxtjs/tailwindcss" />
 /// <reference types="@nuxt/telemetry" />
+/// <reference types="@nuxtjs/tailwindcss" />
 /// <reference path="types/nitro-layouts.d.ts" />
 /// <reference path="types/builder-env.d.ts" />
 /// <reference types="nuxt" />
